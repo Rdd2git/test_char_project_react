2a4bc985f5a8fb56ad10e47e8d2374905612f612
Rdd2git <34942625+Rdd2git@users.noreply.github.com>
2020-02-14 16:35:37 +0300

    Add files via upload

---
+++ build/precache-manifest.deaad836492a8663ff3c4b5d6815c4ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47eec79b71c7421116f38b4ac50cd393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dd9039718ac7509536a",
+    "url": "/static/js/2.7d474251.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/static/js/2.7d474251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae2bc8ece9b1f2da944",
+    "url": "/static/js/main.6af043ab.chunk.js"
+  },
+  {
+    "revision": "afbf8aea6f43e7e8e0cc",
+    "url": "/static/js/runtime-main.626bd2b0.js"
+  }
+]);
